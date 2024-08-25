b847f7724f4e028c4417b58de6db8e2a6cf9a81d
NAlexsandr <sliknike-78@mail.ru>
2024-08-25 16:07:37 +0700

    Lesson1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(`Текст из script.js`);
+let temperatureС = 5;
+let temperatureF = (9 / 5) * temperatureС + 32;
+console.log(`Температура по Цельсию = ${temperatureС}`);
+console.log(`Температура по Фаренгейту = ${temperatureF}`);
+
+let nameMy = `Alexandr`;
+let admin = nameMy;
+console.log(`${admin}`)
