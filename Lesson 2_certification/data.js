11a5cc095f476abd69095a25c05683c461cda084
NAlexsandr <132039918+NAlexsandr@users.noreply.github.com>
2024-11-03 19:09:49 +0700

    LessonCertification

---
+++ Lesson 2_certification/data.js
@@ -0,0 +1,61 @@
+const dataProducts =`[
+  {
+    "id": "1",
+    "url": "./img/card2/Foto1.jpg",
+    "title": "LLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00",
+    "color": "BLUE",
+    "size": "X"
+  },
+
+  {
+    "id": "2",
+    "url": "./img/card2/Foto2.png",
+    "title": "LLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00",
+    "color": "BLACK",
+    "size": "XII"
+  },
+
+  {
+    "id": "3",
+    "url": "./img/card2/Foto3.png",
+    "title": "LLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$25.00",
+    "color": "RED",
+    "size": "XI"
+  },
+
+  {
+    "id": "4",
+    "url": "./img/card2/Foto4.png",
+    "title": "LLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00",
+    "color": "BLUE",
+    "size": "XI"
+  },
+
+  {
+    "id": "5",
+    "url": "./img/card2/Foto5.png",
+    "title": "LLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00",
+    "color": "RED",
+    "size": "XI"
+  },
+
+  {
+    "id": "6",
+    "url": "./img/card2/Foto6.png",
+    "title": "LLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00",
+    "color": "GREEN",
+    "size": "XI"
+  }
+]`
